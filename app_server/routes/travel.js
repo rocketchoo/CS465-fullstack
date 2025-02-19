00603938901bb6b264863ea680eafd44cb9b8675
Jenna Cho <jawoo.cho@snhu.edu>
2025-02-20 02:50:24 +0900

    Initial commit

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/travel');
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/travel');
 
-// GET home page
+/* GET travel page. */
 router.get('/', controller.travel);
 
 module.exports = router;
